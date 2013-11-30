8db7dcce258a7061848dfdc8a373cde2aea21d5f
Roman Gafiatullin <gafiatullin.roman@gmail.com>
2013-11-30 19:18:02 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThreeJsExperiment::Application.config.secret_key_base = '8b96530922d1765ba073ad4ff902723d5dc35944b04dda11fda984b7a86941d98e653d4a9f287e1d9a8ca35861d6d1a23bf09aa3c0cb52e00091d7a193f43e8c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThreeJsExperiment::Application.config.session_store :cookie_store, key: '_three-js-experiment_session'
